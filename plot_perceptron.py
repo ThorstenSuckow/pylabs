106caa74fde7343d92cf0dc6e18f058f220712b2
Thorsten Suckow-Homberg <thorsten@suckow-homberg.de>
2023-07-03 23:04:37 +0200

    refactor: generalize plotting logic

--- plot_perceptron.py
+++ plot_perceptron.py
@@ -1,5 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.offsetbox import AnchoredOffsetbox, TextArea, DrawingArea, HPacker
+from matplotlib.patches import Circle, Ellipse
 
 from Perceptron import Perceptron
 from Vector import Vector
@@ -13,6 +15,9 @@ p = Perceptron(Vector([1, 1]))
 w = p.learn(M_pos, M_neg)
 
 history = p.history()
+
+for entry in history:
+    print(entry)
 
 # Vector origin location
 X = [0]
@@ -36,32 +41,71 @@ plt.xlim(MIN, MAX)
 plt.ylim(MIN, MAX)
 
 
+def draw_plot(step):
+    # draw in negative / positive datasets
+    for set in M_neg:
+        c = ax.plot((set[0]), (set[1]), '.', color='r')
 
-# draw in negative / positive datasets
-for set in M_neg:
-    c = ax.plot((set[0]), (set[1]), '.', color='r')
+    for set in M_pos:
+        c = ax.plot((set[0]), (set[1]), '.', color='g')
 
-for set in M_pos:
-    c = ax.plot((set[0]), (set[1]), '.', color='g')
+    # draw step from first entry in history
+    w = step[0]
+
+    plt.quiver(X, Y, w[0], w[1], color='r', scale=1, units='xy', angles='xy', scale_units='xy', width=0.02)
+    plt.text(3, 2.5, r' $w = \binom{' + str("%.2f" % w[0]) + '}{' + str("%.2f" % w[1]) + '}$', color="r")
+
+    if len(step) > 1:
+        x = step[1]
+        scalar = step[2]
+        result = step[3]
+        processedset = step[4]
+
+        plt.quiver(w[0], w[1], result[0] - w[0], result[1] - w[1], ec='black', linestyle='--', scale=1, units='xy',
+                   angles='xy', scale_units='xy', width=0.02)
+
+        if processedset:
+            c1 = plt.Circle((processedset[0], processedset[1]), 0.1, alpha=0.5, color='orange')
+            ax.add_patch(c1)
+            # data from first set
+
+            box1 = TextArea( r"$x = (" + str("%.2f" % processedset[0]) + ", " + str("%.2f" % processedset[1]) + ")$", textprops=dict(color="k"))
+            box2 = DrawingArea(0, 0, 0, 0)
+            c2 = Circle((0, -5.5), radius=4.5, fc="orange")
+            box2.add_artist(c2)
+            box = HPacker(children=[box2, box1],
+                          align="right",
+                          pad=0, sep=5)
+            anchored_box = AnchoredOffsetbox(loc='lower left',
+                                             child=box, pad=0.,
+                                             frameon=False,
+                                             bbox_to_anchor=(1.02, 0.82),
+                                             bbox_transform=ax.transAxes,
+                                             borderpad=0.,
+                                             )
+            ax.add_artist(anchored_box)
 
 
-# draw step from first entry in history
-step = history[0]
-w = step[0]
-x = step[1]
-scalar = step[2]
-result = step[3]
+        # result
+        plt.text(3, 1.5, r"$\Delta w=(" + str("%.2f" % result[0]) + ", " + str("%.2f" % result[1]) + ")$")
+
+    # negative/positive plane
+    plt.plot(
+        # x_1, x_2, y_1, y_2
+        [MAX * (w[1] / (0 - w[0])), MIN * (w[1] / (0 - w[0]))], [MAX * 1, MIN * 1]
+    )
+    fig.subplots_adjust(right=0.7)
+
+    # Show plot with grid
+    plt.grid()
+    plt.show()
 
 
-plt.quiver(X, Y, w[0], w[1], color='r', alpha=0.5, scale=1, units='xy', angles='xy', scale_units='xy')
-plt.quiver(w[0], w[1],  result[0] - w[0],   result[1] - w[1], ec='black', linestyle='--', linewidth=0.1, alpha=0.5, scale=1, units='xy', angles='xy', scale_units='xy')
+#draw_plot(history[0])
+#draw_plot(history[1])
+#draw_plot(history[2])
 
-# negative/positive plane
-plt.plot(
-    [MAX * w[0], MIN * w[1]],
-    [MIN * w[0], MAX * w[1]]
-)
+#draw_plot(history[3])
+#draw_plot(history[4])
 
-# Show plot with grid
-plt.grid()
-plt.show()
+draw_plot([w.to_array()])
